94533ef0b2cfce666819402091abe736e1dbd916
Ethan <e.thom.butler@gmail.com>
2016-04-01 23:00:56 -0400

    add stringify

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var Markov = require('markov')
 
 http.createServer(function(req, res){
   Tweets.getTweets(function(tweets){
-    var markov = Markov(3)
+    var markov = Markov(2)
     markov.seed(tweets, function(){
       var key = markov.pick()
-      var text = markov.fill(key,8).join(' ')
-      res.writeHead(200, {'Content-Type': 'text/plain'})
-      res.end(text)
+      var text = markov.fill(key,10).join(' ')
+      res.writeHead(200, {'Content-Type': 'application/json'})
+      res.end(JSON.stringify({
+        text: text
+      }))
     })
   })
 }).listen(process.env.PORT || 3000)
